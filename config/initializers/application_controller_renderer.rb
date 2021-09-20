746d87f66983098046a536b3b546c5d7d0defffd
MizutaYuto <90345660+mizutayuto@users.noreply.github.com>
2021-09-20 08:45:21 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '463f093113b2a8f0870c2517264cbc983db0d4b84a2f7db2462beaf062a6d6c2833988d118bf91ece092556230ff79df62706ec978f3e107921de4f81cfc6fa3'
